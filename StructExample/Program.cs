4e7f7d8efdf4b1228ec780293cafeeb7bfeb593b
Richard Chalk <barmybrit@gmail.com>
2023-06-30 10:53:57 +0200

    Add project files.

---
+++ StructExample/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace StructExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // timeS in this example is a 'struct'
+            // and will be COPIED to the method UpdateTime()
+            TimeStruct timeS = new TimeStruct();
+            timeS.Seconds = 10;
+            UpdateTime(timeS);
+
+            // timeC in this example is a 'class'
+            // and a REFERENCE will be sent to the method UpdateTime()
+            TimeStruct timeC = new TimeStruct();
+            timeC.Seconds = 20;
+            UpdateTime(timeC);
+        }
+
+        public static void UpdateTime(TimeStruct time)
+        {
+            time.Seconds++;
+        }
+
+    }
+}
---
+++ StructExample/TimeClass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructExample
+{
+    public class TimeClass
+    {
+        private int seconds;
+        public int Seconds
+        {
+            get { return seconds; }
+            set { seconds = value; }
+        }
+
+        public int CalculateMinutes()
+        {
+            return seconds / 60;
+        }
+    }
+
+}
